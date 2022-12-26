0d8080f162464acbe53e0d2cff9d233d21ea7fea
TheJP <jp_55555@hotmail.com>
2022-12-26 23:07:35 +0100

    Solved Day 1 Part 2

---
+++ day01.2/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]).Select(int.Parse).ToArray();
+
+var previous = int.MaxValue;
+int score = 0;
+for (int i = 3; i <= input.Length; ++i) {
+    var sum = input[(i-3)..i].Sum();
+    if (sum > previous) ++score;
+    previous = sum;
+}
+
+Console.WriteLine($"{score}");
