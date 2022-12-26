3ee07ebdf37fb0d427bbfda42ce8f84632edc431
TheJP <jp_55555@hotmail.com>
2022-12-26 23:05:36 +0100

    Solved Day 1 Part 1

---
+++ day01.1/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]).Select(int.Parse);
+
+var previous = input.First();
+int score = 0;
+foreach (var number in input.Skip(1))
+{
+    if (number > previous) ++score;
+    previous = number;
+}
+
+Console.WriteLine($"{score}");
