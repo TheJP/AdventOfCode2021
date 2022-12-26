4ac8afc9379ac1ccba55caba30eddebbf0cf6707
TheJP <jp_55555@hotmail.com>
2022-12-26 23:53:50 +0100

    Solved Day 2 Part 2

---
+++ day02.2/Program.cs
@@ -0,0 +1,28 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+
+int x = 0;
+int depth = 0;
+int aim = 0;
+
+foreach (var line in input)
+{
+    var parts = line.Split();
+    var amount = int.Parse(parts[1]);
+    switch (parts[0])
+    {
+        case "forward":
+            x += amount;
+            depth += aim * amount;
+            break;
+        case "up":
+            aim -= amount;
+            break;
+        case "down":
+            aim += amount;
+            break;
+        default:
+            throw new InvalidOperationException();
+    }
+}
+
+Console.WriteLine($"{depth * x}");
