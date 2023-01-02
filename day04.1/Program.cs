5f6ef2c428ff672027f938a02ef2b5cc74648c34
TheJP <jp_55555@hotmail.com>
2023-01-02 18:30:11 +0100

    Solved Day 4 Part 1

---
+++ day04.1/Program.cs
@@ -0,0 +1,53 @@
+﻿const int BoardLength = 5;
+var lines = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+
+var numbers = lines.First().Split(',').Select(int.Parse).ToArray();
+
+var boards = lines.Skip(2).Chunk(6).Select(board =>
+    board.Take(5).Select(line =>
+        line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()
+    ).ToArray()
+).ToArray();
+
+var found = new bool[boards.Length, BoardLength, BoardLength];
+
+foreach (var current in numbers)
+{
+    for (int i = 0; i < boards.Length; ++i)
+    {
+        for (int y = 0; y < BoardLength; ++y)
+        {
+            bool victory = true;
+            int addedX = -1;
+            for (int x = 0; x < BoardLength; ++x)
+            {
+                if (boards[i][y][x] == current) { addedX = x; found[i, y, x] = true; }
+                else if (!found[i, y, x]) victory = false;
+            }
+            if (addedX > 0 && !victory)
+            {
+                victory = true;
+                for (int y2 = 0; y2 < BoardLength; ++y2)
+                {
+                    if (!found[i, y2, addedX]) { victory = false; break; }
+                }
+            }
+
+            int score = 0;
+            if (victory)
+            {
+                for (int y2 = 0; y2 < BoardLength; ++y2)
+                {
+                    for (int x = 0; x < BoardLength; ++x)
+                    {
+                        if (!found[i, y2, x]) score += boards[i][y2][x];
+                    }
+                }
+                // Console.WriteLine($"{i}");
+                // Console.WriteLine($"{score} {current}");
+                Console.WriteLine($"{score * current}");
+                return;
+            }
+        }
+    }
+}
