11b1236d76040c77ab347c65365a5bb900375df4
TheJP <jp_55555@hotmail.com>
2023-01-03 11:53:51 +0100

    Solved Day 7 Part 1

---
+++ day07.1/Program.cs
@@ -0,0 +1,11 @@
+﻿var numbers = File.ReadLines(Environment.GetCommandLineArgs()[1])
+    .First().Split(',').Select(int.Parse).ToArray();
+
+// Median is the solution
+Array.Sort(numbers);
+int half = numbers.Length / 2;
+int best = numbers.Length % 2 == 1 ? numbers[half] : ((numbers[half] + numbers[half]) / 2);
+
+int fuel = numbers.Select(x => Math.Abs(best - x)).Sum();
+// Console.WriteLine(best);
+Console.WriteLine(fuel);
