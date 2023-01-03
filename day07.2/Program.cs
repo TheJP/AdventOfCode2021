1b2da79e6a22c4f40ef5e91d6210f35f9bcad922
TheJP <jp_55555@hotmail.com>
2023-01-03 12:00:44 +0100

    Solved Day 7 Part 2

---
+++ day07.2/Program.cs
@@ -0,0 +1,13 @@
+﻿var numbers = File.ReadLines(Environment.GetCommandLineArgs()[1])
+    .First().Split(',').Select(int.Parse).ToArray();
+
+int best = int.MaxValue;
+int max = numbers.Max();
+
+for (int i = numbers.Min(); i <= max; ++i)
+{
+    int fuel = numbers.Select(x => { var n = Math.Abs(i - x); return (n * (n + 1)) / 2; }).Sum();
+    best = Math.Min(best, fuel);
+}
+
+Console.WriteLine(best);
