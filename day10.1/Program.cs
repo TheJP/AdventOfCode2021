2595a1e1e3ec7b1e1fb670321dcb93021f797a97
TheJP <jp_55555@hotmail.com>
2023-01-04 00:54:30 +0100

    Solved Day 10 Part 1

---
+++ day10.1/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+
+var map = new Dictionary<char, (char, int)>()
+{
+    { '(', (')', 3) },
+    { '[', (']', 57) },
+    { '{', ('}', 1197) },
+    { '<', ('>', 25137) },
+};
+var scoreTable = map.ToDictionary(pair => pair.Value.Item1, pair => pair.Value.Item2);
+
+int score = 0;
+foreach (var line in input)
+{
+    var stack = new Stack<char>();
+    foreach (var c in line)
+    {
+        if (map.ContainsKey(c)) stack.Push(c);
+        else
+        {
+            var top = stack.Pop();
+            if (c == map[top].Item1) continue;
+
+            score += scoreTable[c];
+            break;
+        }
+    }
+}
+
+Console.WriteLine(score);
