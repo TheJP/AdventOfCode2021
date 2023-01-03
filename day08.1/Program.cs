d1ae5991d9e06254be82e4432fbf0a1985949299
TheJP <jp_55555@hotmail.com>
2023-01-03 12:13:08 +0100

    Solved Day 8 Part 1

---
+++ day08.1/Program.cs
@@ -0,0 +1,13 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+
+var uniqueLenghts = new HashSet<int>() { 2, 4, 3, 7 };
+
+int score = 0;
+foreach (var line in input)
+{
+    var parts = line.Split('|');
+    var segments = parts[1].Split();
+    score += segments.Count(s => uniqueLenghts.Contains(s.Length));
+}
+
+Console.WriteLine(score);
